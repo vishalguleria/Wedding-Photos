def9a53ff0ebf67e3f20e7e7c088e32f9a912b41
vishalguleria <51770483+vishalguleria@users.noreply.github.com>
2025-10-25 02:16:06 +0530

    Update make_images_json

--- make_images_json.py
+++ make_images_json.py
@@ -2,7 +2,7 @@
 # Usage: put your images in a local 'images' folder, set BASE_URL to the raw hosting URL (e.g. GitHub raw URL),
 # then run: python3 make_images_json.py
 import os, json
-BASE_URL = "https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/YOUR_REPO/main/images/"  # <-- EDIT
+BASE_URL = "https://raw.githubusercontent.com/vishalguleria/Wedding-Photos/main/images/"  # <-- EDIT
 LOCAL_IMAGES_DIR = "images"
 
 images = []
